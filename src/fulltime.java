3762e42b82ece0b5e9e35eac07c1264d299799fc
unknown <stkey002@gmail.com>
2023-09-29 01:58:33 +0700

    update function

--- src/fulltime.java
+++ src/fulltime.java
@@ -9,10 +9,12 @@ class fulltime extends nhanVien {
         if (working_time < 0) {
             System.out.println("working time input < 0");
         } else if (working_time < 40) {
+            this.working_time = working_time;
             System.out.println("not enough working time");
         } else {
             this.working_time = working_time;
         }
     }
+
 }
 
--- src/parttime.java
+++ src/parttime.java
@@ -8,6 +8,7 @@ class parttime extends nhanVien {
         if (working_time < 0) {
             System.out.println("working time input < 0");
         } else if (working_time < 20) {
+            this.working_time = working_time;
             System.out.println("not enough working time");
         } else {
             this.working_time = working_time;
